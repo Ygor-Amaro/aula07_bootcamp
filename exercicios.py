eb76399ef5411ade984df79ed065a657b5301221
Ygor Amaro <ykba09@gmail.com>
2025-05-18 21:10:42 -0300

    exercicio 01 - ok

--- exercicios.py
+++ exercicios.py
@@ -1,5 +1,21 @@
 # 1 - Calcular Média de Valores em uma Lista
 
+teste = [1, 2, 3, 4, 5]
+
+def media(lista: list) -> float:
+    """
+    Calcula a média de uma lista de números.
+    Parâmetros:
+        lista (list): Lista de números.
+    Retorna:
+        float: Média dos números na lista.
+    """
+    if not lista:
+        return 0
+    return sum(lista) / len(lista)
+
+media(teste)
+print(f"Média: {media(teste)}")
 
 # 2 - Filtrar Dados Acima de um Limite
 
