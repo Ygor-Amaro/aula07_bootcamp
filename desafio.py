142cfa7ebd9d303c59e9cd38cd2353aeffe2a9a5
Ygor Amaro <ykba09@gmail.com>
2025-05-18 22:12:39 -0300

    ler csv ok

--- desafio.py
+++ desafio.py
@@ -1,25 +1,27 @@
-"""Desafio: Análise de Vendas de Produtos Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, 
+"""
+Desafio: Análise de Vendas de Produtos Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, 
  o desafio consiste em ler os dados, processá-los em um dicionário para análise e, por fim, calcular e reportar as vendas totais por categoria de produto.
  """
 
-#Tarefas:
-#   Ler o arquivo CSV e carregar os dados.
-#   Processar os dados em um dicionário, onde a chave é a categoria, e o valor é uma lista de dicionários, cada um contendo informações do produto (Produto, Quantidade, Venda).
-#   Calcular o total de vendas (Quantidade * Venda) por categoria.
-    # Funções
-    #   Ler CSV:
-    #       Função: ler_csv
-    #       Entrada: Nome do arquivo CSV
-    #       Saída: Lista de dicionários com dados lidos
-    #   Processar Dados:
-    #       Função: processar_dados
-    #       Entrada: Lista de dicionários
-    #       Saída: Dicionário processado conforme descrito
-    #   Calcular Vendas por Categoria:
-    #       Função: calcular_vendas_categoria
-    #       Entrada: Dicionário processado
-    #       Saída: Dicionário com total de vendas por categoria
-
+""""
+Tarefas:
+   Ler o arquivo CSV e carregar os dados.
+   Processar os dados em um dicionário, onde a chave é a categoria, e o valor é uma lista de dicionários, cada um contendo informações do produto (Produto, Quantidade, Venda).
+   Calcular o total de vendas (Quantidade * Venda) por categoria.
+     Funções
+       Ler CSV:
+           Função: ler_csv
+           Entrada: Nome do arquivo CSV
+           Saída: Lista de dicionários com dados lidos
+       Processar Dados:
+           Função: processar_dados
+           Entrada: Lista de dicionários
+           Saída: Dicionário processado conforme descrito
+       Calcular Vendas por Categoria:
+           Função: calcular_vendas_categoria
+           Entrada: Dicionário processado
+           Saída: Dicionário com total de vendas por categoria
+"""
 
 def ler_csv(arquivo):
     """
