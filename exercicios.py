fa327e363d636cd10642e040677175e1f37ef912
Ygor Amaro <ykba09@gmail.com>
2025-05-18 21:01:01 -0300

    exercicios escritos

---
+++ exercicios.py
@@ -0,0 +1,17 @@
+# 1 - Calcular Média de Valores em uma Lista
+
+
+# 2 - Filtrar Dados Acima de um Limite
+
+
+# 3 - Contar Valores Únicos em uma Lista
+
+
+# 4 - Converter Celsius para Fahrenheit em uma Lista
+
+
+# 5 - Calcular Desvio Padrão de uma Lista
+
+
+# 6 - Encontrar Valores Ausentes em uma Sequência
+
