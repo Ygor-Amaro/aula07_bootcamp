394fa7a761db142dca9971b6e054fed9777c43c3
Ygor Amaro <ykba09@gmail.com>
2025-05-18 21:46:01 -0300

    exercicio 02 - ok

--- exercicios.py
+++ exercicios.py
@@ -1,24 +1,38 @@
 # 1 - Calcular Média de Valores em uma Lista
 
-teste = [1, 2, 3, 4, 5]
+# teste = [1, 2, 3, 4, 5]
 
-def media(lista: list) -> float:
-    """
-    Calcula a média de uma lista de números.
-    Parâmetros:
-        lista (list): Lista de números.
-    Retorna:
-        float: Média dos números na lista.
-    """
-    if not lista:
-        return 0
-    return sum(lista) / len(lista)
+# def media(lista: list) -> float:
+#     """
+#     Calcula a média de uma lista de números.
+#     Parâmetros:
+#         lista (list): Lista de números.
+#     Retorna:
+#         float: Média dos números na lista.
+#     """
+#     if not lista:
+#         return 0
+#     return sum(lista) / len(lista)
 
-media(teste)
-print(f"Média: {media(teste)}")
+# media(teste)
+# print(f"Média: {media(teste)}")
 
 # 2 - Filtrar Dados Acima de um Limite
 
+teste = [1, 2, 3, 4, 5]
+
+def filtro(lista: list[float], limite: float) -> list[float]:
+    """
+    Filtra os valores de uma lista que estão acima de um limite.
+    Parâmetros:
+        lista (list): Lista de números.
+        limite (float): Limite para filtrar os números.
+    Retorna:
+        list: Lista com os números acima do limite.
+    """
+    return [x for x in lista if x > limite]
+
+print(filtro(teste, 3.5))
 
 # 3 - Contar Valores Únicos em uma Lista
 
